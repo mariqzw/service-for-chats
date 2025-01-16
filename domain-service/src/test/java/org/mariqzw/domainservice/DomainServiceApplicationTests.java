6f9d2864296cd82d6cec2e830bc2829ebccd04e1
Maria <masha.podrez@mail.ru>
2025-01-16 23:25:12 +0300

    Update dependencies and remove test

--- domain-service/src/test/java/org/mariqzw/domainservice/DomainServiceApplicationTests.java
+++ domain-service/src/test/java/org/mariqzw/domainservice/DomainServiceApplicationTests.java
@@ -3,11 +3,11 @@ package org.mariqzw.domainservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class DomainServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class DomainServiceApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
