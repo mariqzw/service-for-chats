2e5096547765a9e70dc0dcae660e742fa4720902
AngryL1on <vadim.angryl1on@gmail.com>
2025-01-16 16:18:43 +0300

    Init domain service

---
+++ domain-service/src/test/java/org/mariqzw/domainservice/DomainServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mariqzw.domainservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DomainServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
