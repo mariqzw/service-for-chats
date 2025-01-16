165472235cf5ff0abbb46655fe590b1e6781c1d1
AngryL1on <vadim.angryl1on@gmail.com>
2025-01-16 17:49:48 +0300

    Finish work in the gateway service

---
+++ gateway/src/main/java/org/mariqzw/gateway/models/dtos/MessageDTO.java
@@ -0,0 +1,61 @@
+package org.mariqzw.gateway.models.dtos;
+
+public class MessageDTO {
+
+    private Long id;
+    private String sender;
+    private String receiver;
+    private String messageText;
+    private String timestamp;
+
+    public MessageDTO() {
+    }
+
+    public MessageDTO(Long id, String sender, String receiver, String messageText, String timestamp) {
+        this.id = id;
+        this.sender = sender;
+        this.receiver = receiver;
+        this.messageText = messageText;
+        this.timestamp = timestamp;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(String receiver) {
+        this.receiver = receiver;
+    }
+
+    public String getMessageText() {
+        return messageText;
+    }
+
+    public void setMessageText(String messageText) {
+        this.messageText = messageText;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+}
