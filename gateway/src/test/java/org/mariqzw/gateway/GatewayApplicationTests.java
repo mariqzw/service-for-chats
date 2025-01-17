c3ef348cbc5e1377f7171ecd00b96bbbb4a6ef38
Maria <masha.podrez@mail.ru>
2025-01-17 19:40:18 +0300

    Add settings and dependencies for RMQ in gateway

--- gateway/src/test/java/org/mariqzw/gateway/GatewayApplicationTests.java
+++ gateway/src/test/java/org/mariqzw/gateway/GatewayApplicationTests.java
@@ -3,11 +3,11 @@ package org.mariqzw.gateway;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class GatewayApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//@SpringBootTest
+//class GatewayApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
